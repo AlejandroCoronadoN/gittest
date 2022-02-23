fa169a51b2452ced4beb04e713619ff60679e1fe
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2022-02-23 16:39:24 -0600

    another function

--- sharedfile.py
+++ sharedfile.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+def another_fun():
+    """Pritnts stuff
+    """
+    print('stuff')
+    
+    
 def shared_fun_toto(teni:int):
     """Calculates the numeber of kiloemters you can run given the 
     number of teni in your stock
@@ -12,6 +18,8 @@ def shared_fun_toto(teni:int):
         return 0 
     else:
         return teni*1000
+    
+    
 def shared_fun(pizza:int, burgers:int):
     """A function that multiplies burgers and pizzas in order to obtain
     the area under the curve of your belly.
@@ -21,6 +29,7 @@ def shared_fun(pizza:int, burgers:int):
         burgers (int): number of burguers
     """
     return pizza*burgers +2
+
 
 def new_fun(diccionario:dict):
     """Iterates trough dictionary
